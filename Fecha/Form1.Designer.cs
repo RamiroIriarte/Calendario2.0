897a2a734558e79df19dd0b7fc1c4474023b924f
Ramiro Iriarte <ramiro_iriarte003@hotmail.com>
2023-06-29 01:03:23 -0300

    Agregar archivos de proyecto.

---
+++ Fecha/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Fecha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.btmAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.epvValidar = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.epvValidar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(73, 70);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 15);
+            this.lblFecha.TabIndex = 0;
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(2, 23);
+            this.txtDia.MaxLength = 2;
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(33, 20);
+            this.txtDia.TabIndex = 1;
+            this.txtDia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDia_KeyPress);
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(76, 23);
+            this.txtMes.MaxLength = 2;
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(33, 20);
+            this.txtMes.TabIndex = 2;
+            this.txtMes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMes_KeyPress);
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(159, 23);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(60, 20);
+            this.txtAño.TabIndex = 3;
+            this.txtAño.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAño_KeyPress);
+            // 
+            // btmAceptar
+            // 
+            this.btmAceptar.Location = new System.Drawing.Point(236, 23);
+            this.btmAceptar.Name = "btmAceptar";
+            this.btmAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btmAceptar.TabIndex = 4;
+            this.btmAceptar.Text = "Aceptar";
+            this.btmAceptar.UseVisualStyleBackColor = true;
+            this.btmAceptar.Click += new System.EventHandler(this.btmAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(128, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "/";
+            // 
+            // epvValidar
+            // 
+            this.epvValidar.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 153);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btmAceptar);
+            this.Controls.Add(this.txtAño);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.lblFecha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epvValidar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.Button btmAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider epvValidar;
+    }
+}
+
---
+++ Fecha/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fecha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void btmAceptar_Click(object sender, EventArgs e)
+        {
+            //Limpieza de error provider
+
+            epvValidar.Clear();
+
+            //Validacion de llenado de campos
+
+            if (Validar())
+            {
+
+            }
+            else
+            {
+
+                //creacion de variables y dandole un valor
+
+                string mescrito = "";
+                int dia = Convert.ToInt32(txtDia.Text);
+                int mes = Convert.ToInt32(txtMes.Text);
+                int año = Convert.ToInt32(txtAño.Text);
+
+
+                if (mes > 12 || mes < 1)
+                {
+                    MessageBox.Show("Mes no Valido", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else
+                {
+                    //Se revisan los meses para agregar un dia, un mes o un año
+
+                    if (dia == 31 && (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12))
+                    {
+                        if ((dia == 31) && (mes == 12))
+                        {
+                            año = año + 1;
+                            mes = 0;
+
+                        }
+
+                        dia = 1;
+                        mes = mes + 1;
+
+                    }
+                    else if (dia == 30 && (mes == 4 || mes == 6 || mes == 9 || mes == 11))
+                    {
+                        dia = 1;
+                        mes = mes + 1;
+                    }
+                    else if (dia == 28 && mes == 3)
+                    {
+                        dia = 1;
+                        mes = mes + 1;
+                    }
+                    else
+                    {
+                        dia = dia + 1;
+                    }
+
+                    //Se agarra el valor de mes y se lo pasa a un string
+
+                    if (mes == 1)
+                    {
+                        mescrito = "Enero";
+                    }
+                    else if (mes == 2)
+                    {
+                        mescrito = "Febrero";
+                    }
+                    else if (mes == 3)
+                    {
+                        mescrito = "Marzo";
+                    }
+                    else if (mes == 4)
+                    {
+                        mescrito = "Abril";
+                    }
+                    else if (mes == 5)
+                    {
+                        mescrito = "Mayo";
+                    }
+                    else if (mes == 6)
+                    {
+                        mescrito = "Junio";
+                    }
+                    else if (mes == 7)
+                    {
+                        mescrito = "Julio";
+                    }
+                    else if (mes == 8)
+                    {
+                        mescrito = "Agosto";
+                    }
+                    else if (mes == 9)
+                    {
+                        mescrito = "Septiembre";
+                    }
+                    else if (mes == 10)
+                    {
+                        mescrito = "Octubre";
+                    }
+                    else if (mes == 11)
+                    {
+                        mescrito = "Noviembre";
+                    }
+                    else if (mes == 12)
+                    {
+                        mescrito = "Diciembre";
+                    }
+
+                    txtDia.Text = "";
+                    txtAño.Text = "";
+                    txtMes.Text = "";
+
+                    lblFecha.Text = Convert.ToString(dia) + " de " + mescrito + " de " + Convert.ToString(año);
+                }
+
+
+            }
+
+        }
+
+        //Validacion para que solo se puedan poner numeros en los text box
+
+        private void txtDia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("solo numeros", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtMes_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("solo numeros", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtAño_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("solo numeros", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private bool Validar()
+        {
+            bool validar = false;
+            if (txtAño.Text == "")
+            {
+                epvValidar.SetError(txtAño, "Llenar campo");
+                validar = true;
+            }
+            if (txtMes.Text == "")
+            {
+                epvValidar.SetError(txtMes, "Llenar campo");
+                validar = true;
+            }
+            if (txtDia.Text == "")
+            {
+                epvValidar.SetError(txtDia, "Llenar campo");
+                validar = true;
+
+            }
+            return validar;
+        }
+    }
+}
